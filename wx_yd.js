c7d7631fa3213e0f6462eea93d871ac2ef37d4be
acone555 <82928956+acone555@users.noreply.github.com>
2023-03-11 09:45:45 +0800

    Update wx_yd.js

--- wx_yd.js
+++ wx_yd.js
@@ -1,28 +1,19 @@
 /*
-青龙薅羊毛-微信阅读
+微信阅读
+需要青龙环境
+入口，微信打开 -> https://zl0227233846-1316104230.cos.ap-nanjing.myqcloud.com/index.html?upuid=10327150
+抓包m.*.shop域名下cookie,填入环境变量 yuedu，多账户换行隔开
 
-在微信中打开  https://zl0114225029-1314804847.cos.ap-nanjing.myqcloud.com/index.html?upuid=10688156
-
-打开后自动转跳阅读文章 空白页关闭后 重新打开 
-打开文章停5秒 手动返回上一页
-
-青龙需要抓包m.*.shop域名下cookie,
-环境变量-新建变量 名称: wxyd  值:(你抓取的cookie)
-多个账户回车换行隔开
-
-每天会验证2次左右，碰到会plus推送提醒
-手动在微信里打开上面的链接看两篇文章 
-出现 阅读成功,金币增加 的提示即可
-
-每篇文章120金币 每3000金自动提现0.3元 
-每小时30篇 每天6轮 180篇文章共21600金币=2.16
+每天会验证2次左右，碰到验证文章手动打开看一篇即可
+当前每日30篇*6轮180篇文章约2.2元
+会自动提现
 */
 
 const $ = new Env("微信阅读");
 const notify = $.isNode() ? require('./sendNotify') : '';
 let envSplitor = ['@', '\n']
 let httpResult, httpReq, httpResp
-let ckName = 'wxyd'
+let ckName = 'yuedu'
 let userCookie = ($.isNode() ? process.env[ckName] : $.getdata(ckName)) || '';
 let userList = []
 let userIdx = 0
